81ca5c374f001f224cb4b1f6144f1cbd2f4096a6
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-15 22:34:54 +0300

    added login and home routes

--- scout-ai-react/src/App.jsx
+++ scout-ai-react/src/App.jsx
@@ -1,8 +1,18 @@
+import Home from './pages/Home';
+import Login from './pages/Login';
 import './styles/App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/home' element={<Home />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
