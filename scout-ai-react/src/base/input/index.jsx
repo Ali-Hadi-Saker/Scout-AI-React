40ac7449b194c72453838bfa5e3c3d0220cfa39f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 01:15:16 +0300

    added handleEmail function

--- scout-ai-react/src/base/input/index.jsx
+++ scout-ai-react/src/base/input/index.jsx
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './style.css'
 
 
-const Input = ({icon, placeholder}) => {
+const Input = ({icon, placeholder, onTextChange}) => {
     return (
         <div className='input-container full-width flex '>
             <FontAwesomeIcon icon={icon} className='input-icon'/>
-            <input type="text"
+            <input 
+            type="text"
+            onChange={(e) => {onTextChange(e)}}
             placeholder={placeholder} />
         </div>
     )
--- scout-ai-react/src/pages/Login/index.jsx
+++ scout-ai-react/src/pages/Login/index.jsx
@@ -7,14 +7,26 @@ import './style.css';
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const handleEmailChange = (e) => {
+        const newEmail = e.target.value;
+        setEmail(newEmail);
+        console.log("Email:", newEmail);
+    }
     return (
         <div className="flex column center page primary-bg">
             <div className="form-container flex column">
                 <h3>Hello,<br/> Welcome Back</h3>
                 <Input 
+                onTextChange={
+                    handleEmailChange
+                }
                 icon={faEnvelope}
                 placeholder={'Email'}/>
                 <Input 
+                onTextChange={(e) => {
+                    setPassword(e.target.value)
+                }}
                 icon={faLock}
                 placeholder={'password'}/>
                 <Button/>
