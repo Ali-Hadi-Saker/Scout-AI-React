c750cb67cef35e6075765dd5ffad567f54475d10
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-15 23:56:49 +0300

    updated input component to be dynamic

--- scout-ai-react/src/base/index.jsx
+++ scout-ai-react/src/base/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './style.css'
 
 
-const Input = () => {
+const Input = ({icon, placeholder}) => {
     return (
         <div className='input-container flex '>
-            <FontAwesomeIcon icon={faEnvelope} className='input-icon'/>
+            <FontAwesomeIcon icon={icon} className='input-icon'/>
             <input type="text"
-            placeholder="Email" />
+            placeholder={placeholder} />
         </div>
     )
 }
--- scout-ai-react/src/pages/Login/index.jsx
+++ scout-ai-react/src/pages/Login/index.jsx
@@ -1,10 +1,13 @@
 import Input from "../../base";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
     return (
         <div className="flex column center page primary-bg">
             <h3>Hello,<br/> Welcome Back</h3>
-            <Input/>
+            <Input 
+            icon={faEnvelope}
+            placeholder={'Email'}/>
         </div>
     )
 }
