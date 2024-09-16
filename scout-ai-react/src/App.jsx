c9f3e3bd43998460537f23f2233e374427f8324f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 21:08:05 +0300

    uodated login page route

--- scout-ai-react/src/App.jsx
+++ scout-ai-react/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Login />} />
           <Route path='/home' element={<Home />} />
 
         </Routes>
