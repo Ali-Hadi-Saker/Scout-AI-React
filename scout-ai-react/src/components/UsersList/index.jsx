f5447a2630d7d605c6d57b8735746fca70859d7a
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 18:53:48 +0300

    updated search-filter function

--- scout-ai-react/src/components/UsersList/index.jsx
+++ scout-ai-react/src/components/UsersList/index.jsx
@@ -1,60 +1,68 @@
 import { useEffect, useState } from "react";
 import { userRemote } from "../../data_source/remote/user_remote";
 import UserCard from "../UserCard";
-import './style.css'
+import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 const UsersList = () => {
-    const [users, setUsers] = useState([])
-    const [searchUser, setSearchUser] = useState('')
-    const fetchUsers = async () =>{
+    const [users, setUsers] = useState([]);
+    const [searchUser, setSearchUser] = useState(''); 
+
+    const fetchUsers = async () => {
         try {
-            const data = await userRemote.getAllUsers()
-            setUsers(data.users)
+            const data = await userRemote.getAllUsers();
+            setUsers(data.users);
         } catch (error) {
             console.log(error);
-            
         }
-    }
+    };
 
-    useEffect(()=>{
-        fetchUsers()
-    },[])
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-    const handleDeleteUser = async (id) =>{
+    const handleDeleteUser = async (id) => {
         try {
-            const {response} = await userRemote.deleteUser(id)
-            console.log(response)
+            const response = await userRemote.deleteUser(id);
             setUsers(users.filter(user => user._id !== id));
-            console.log("delete user");
-
+            console.log("User deleted:", response);
         } catch (error) {
-            console.log(error);            
-        }        
-    }
+            console.log(error);
+        }
+    };
 
-    const filteredUser = users.filter((user) => {
-        user.fname.includes(searchUser)
-    })
-    return(
+    const filteredUsers = users.filter((user) => 
+        user.fname.toLowerCase().includes(searchUser.toLowerCase()) ||
+        user.email.toLowerCase().includes(searchUser.toLowerCase())
+    );
+
+    return (
         <div className="flex column full-width user-list">
             <div className='search-container flex gap rounded'>
-                <FontAwesomeIcon icon={faSearch} className='search-icon'/>
-                <input 
-                    type="text" 
-                    placeholder="Search for User " 
-                    onChange={(e) => setSearchUser(e.target.value)}/>
+                <FontAwesomeIcon icon={faSearch} className='search-icon' />
+                <input
+                    type="text"
+                    placeholder="Search for User"
+                    value={searchUser}
+                    onChange={(e) => setSearchUser(e.target.value)} 
+                />
             </div>
-            {filteredUser.map((user)=>{
-                return (
+
+            {filteredUsers.length > 0 ? (
+                filteredUsers.map((user) => (
                     <UserCard
+                        key={user._id}
                         email={user.email}
                         name={user.fname}
-                        onDeleteClick={() => handleDeleteUser(user._id)}/>
-                )
-            })}
-        </div>      
-    )
-}
+                        onDeleteClick={() => handleDeleteUser(user._id)}
+                    />
+                ))
+            ) : (
+                <p>No users found.</p>
+            )}
+        </div>
+    );
+};
 
-export default UsersList
+export default UsersList;
