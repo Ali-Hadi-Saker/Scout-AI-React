e57aa712e57a0f5348084749a1d538b6e115f281
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 16:08:52 +0300

    added dashbord options

--- scout-ai-react/src/components/sidebar/index.jsx
+++ scout-ai-react/src/components/sidebar/index.jsx
@@ -1,8 +1,16 @@
 import './style.css'
 const Sidebar = () => {
     return(
-        <div className="sidebar flex column full-height">
-            <h2>sidebar</h2>
+        <div className="sidebar flex column full-height grey-bg">
+            <h2>Dashbord</h2>
+            <ul className='sidebar-list flex column full-height bold'>
+                <li className='option-bg clickable'>
+                    Users
+                </li>
+                <li className='option-bg clickable'>
+                    Cars
+                </li>
+            </ul>
         </div>
     )
 } 
