ec91ef1e7371ab42d8035feec76dbf7e4eb820e2
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 13:58:16 +0300

    added style for navbar

--- scout-ai-react/src/components/navbar/index.jsx
+++ scout-ai-react/src/components/navbar/index.jsx
@@ -1,14 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import './style.css'
 const NavBar = ()=>{
     return(
-        <div className='flex'>
+        <div className='flex navbar-container'>
             <h2>Admin Panel</h2>
             <div>
                 <FontAwesomeIcon icon={faSearch} />
                 <input type="text" placeholder="Search ... " />
             </div>
-            <div className='navbar-profile'>
+            <div className='navbar-profile flex'>
                 <FontAwesomeIcon icon={faUser}/>
                 <p>Name</p>
                 <div className='drop-down'>
