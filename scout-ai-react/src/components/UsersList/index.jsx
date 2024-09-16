1fb9787f845c3200a50c79cce923cd6df6a871db
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 18:49:04 +0300

    added filter-user function

--- scout-ai-react/src/components/UsersList/index.jsx
+++ scout-ai-react/src/components/UsersList/index.jsx
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 const UsersList = () => {
     const [users, setUsers] = useState([])
+    const [searchUser, setSearchUser] = useState('')
     const fetchUsers = async () =>{
         try {
             const data = await userRemote.getAllUsers()
@@ -28,18 +29,23 @@ const UsersList = () => {
             console.log("delete user");
 
         } catch (error) {
-            console.log(error);
-            
-        }
-        
+            console.log(error);            
+        }        
     }
+
+    const filteredUser = users.filter((user) => {
+        user.fname.includes(searchUser)
+    })
     return(
         <div className="flex column full-width user-list">
             <div className='search-container flex gap rounded'>
                 <FontAwesomeIcon icon={faSearch} className='search-icon'/>
-                <input type="text" placeholder="Search for User " />
+                <input 
+                    type="text" 
+                    placeholder="Search for User " 
+                    onChange={(e) => setSearchUser(e.target.value)}/>
             </div>
-            {users.map((user)=>{
+            {filteredUser.map((user)=>{
                 return (
                     <UserCard
                         email={user.email}
