2e0d09fa69fab1259ac7c3c52fe8e5412e668a36
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 16:56:36 +0300

    added getAllUsers function

--- scout-ai-react/src/data_source/remote/user_remote.js
+++ scout-ai-react/src/data_source/remote/user_remote.js
@@ -3,6 +3,12 @@ import {token, token} from '../local/token'
 
 export const userRemote = {
     getAllUsers: async() => {
-        
+        const token = token.getToken()
+        const {data} = await axios.get("http://localhost:8080/users/",{
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        return data
     }
 }
