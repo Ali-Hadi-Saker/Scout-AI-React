3f3c2f5708a54a0c07cb5c074860bec2686c8da7
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 12:04:07 +0300

    added onClick functionality for button

--- scout-ai-react/src/base/button/index.jsx
+++ scout-ai-react/src/base/button/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
 
-const Button = () => {
+const Button = ({onMousseClick}) => {
     return (
-    <button
-        className={`flex center rounded clickable bold full-width button`}
-    >
+    <button 
+        className={`flex center rounded clickable bold full-width button`} 
+        onClick={onMousseClick}>
         Login
     </button>
     );
