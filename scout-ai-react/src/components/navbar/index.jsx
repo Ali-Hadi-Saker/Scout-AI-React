91f042e68b14ebb14e8e6c869a2e2ab62ace691f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 14:12:16 +0300

    updated search container style

--- scout-ai-react/src/components/navbar/index.jsx
+++ scout-ai-react/src/components/navbar/index.jsx
@@ -1,15 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 import './style.css'
+import React from "react";
+
 const NavBar = ()=>{
     return(
         <div className='flex navbar-container'>
             <h2>Admin Panel</h2>
-            <div>
-                <FontAwesomeIcon icon={faSearch} />
+            <div className='search-container flex center gap rounded'>
+                <FontAwesomeIcon icon={faSearch} className='search-icon'/>
                 <input type="text" placeholder="Search ... " />
             </div>
-            <div className='navbar-profile flex'>
+            <div className='navbar-profile flex center gap '>
                 <FontAwesomeIcon icon={faUser}/>
                 <p>Name</p>
                 <div className='drop-down'>
