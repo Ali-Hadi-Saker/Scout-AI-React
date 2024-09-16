e0a8ed2bfc41b2d770ead8894d8a55a33d184cf1
Ali-Hadi-Saker <alihady530@gmail.com>
2024-09-16 16:27:55 +0300

    added main parts of user-card

--- scout-ai-react/src/components/UserCard/index.jsx
+++ scout-ai-react/src/components/UserCard/index.jsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faRemove  } from '@fortawesome/free-solid-svg-icons';
 const UserCard = () => {
     return(
-        <div>
-            user card
+        <div className="card-container flex">
+            <FontAwesomeIcon icon={faUser}/>
+            <p>username</p>
+            <p>email@gmail.com</p>
+            <p>status</p>
+            <FontAwesomeIcon icon={faRemove}/>
         </div>
     )
 }
